57472948dee79bb23779edfb2c04936baad26e51
bogoizbranniy <82285384+bogoizbranniy@users.noreply.github.com>
2023-06-09 16:42:24 +0300

    add new input method

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
 
-	std::string input = "A>(B*C+D^E*(A+E))";
+	std::string input = "A+B*C";
 
 	std::vector <std::vector<bool>> vec = Boolean::truth_table(input);
 
@@ -23,7 +23,30 @@ int main() {
 
 	// Return simplified expression
 
+	std::cout << std::endl << "Zhegalkin polynomial: " << Boolean::polynom(input) << std::endl;
+
+	// Return Zhegalkin polynomial
+
+	input = "x0+x1*x2";
+
+	vec = Boolean::truth_table(input);
+
+	std::cout << std::endl << "x0 x1 x2 F" << std::endl;
+
+	for (int i = 0; i < vec.size(); i++) {
+		for (int j = 0; j < vec[0].size(); j++)
+			std::cout << vec[i][j] << "  ";
+		std::cout << std::endl;
+	}
+
+	// Return the truth table in the vector type
+
+	std::cout << std::endl << "Simplify: " << Boolean::simplify(input) << std::endl;
+
+	// Return simplified expression
+
 	std::cout << std::endl << "Zhegalkin polynomial: " << Boolean::polynom(input);
 
 	// Return Zhegalkin polynomial
+
 }
