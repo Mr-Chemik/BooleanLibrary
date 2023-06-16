d89044366ec16bdf5050085d39846d1656b99ec9
GORDEY <82285384+gordeypiter@users.noreply.github.com>
2023-06-17 02:05:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,50 +5,48 @@
 
 int main() {
 
-	std::string input = "A>!(0*B+C)^A";
+	std::string input = "(0*A+!B^D)>(A*!C+D)";
 
-	std::vector <std::vector<bool>> vec = Boolean::truth_table(input);
+	std::vector <std::vector<bool>> table = Boolean::truth_table(input);
+	std::cout << "Truth table: " << '\n' << "A B C D F" << '\n';
 
-	std::cout << "A B C F" << std::endl;
-
-	for (int i = 0; i < vec.size(); i++) {
-		for (int j = 0; j < vec[0].size(); j++)
-			std::cout << vec[i][j] << " ";
-		std::cout << std::endl;
+	for (int i = 0; i < table.size(); i++) {
+		for (int j = 0; j < table[0].size(); j++)
+			std::cout << table[i][j] << " ";
+		std::cout << '\n';
 	}
-
 	// Return the truth table in the vector type
 
-	std::cout << std::endl << "Simplify: " << Boolean::simplify(input) << std::endl;
 
-	// Return simplified expression
+	std::vector <std::vector<std::string>> karnaugh_map = Boolean::karnaugh(input);
+	std::cout << '\n' << "Karnaugh map: " << '\n';
 
-	std::cout << std::endl << "Zhegalkin polynomial: " << Boolean::polynom(input) << std::endl;
+	for (int i = 0; i < karnaugh_map.size(); i++) {
+		for (int j = 0; j < karnaugh_map[0].size(); j++) {
 
-	// Return Zhegalkin polynomial
+			if (j != 0 && i != 0) {
+				for (int k = 0; k < karnaugh_map[0][1].size() - 1; k++)
+					std::cout << " ";
+			}
 
+			if (j == 1 && i != 0) {
+				for (int k = 0; k < karnaugh_map[0][1].size() + 1; k++)
+					std::cout << " ";
+			}
 
+			std::cout << karnaugh_map[i][j] << " ";
+		}
 
-	input = "x0>!(0*x1+x2)^x0";
-
-	vec = Boolean::truth_table(input);
-
-	std::cout << std::endl << "x0 x1 x2 F" << std::endl;
-
-	for (int i = 0; i < vec.size(); i++) {
-		for (int j = 0; j < vec[0].size(); j++)
-			std::cout << vec[i][j] << "  ";
-		std::cout << std::endl;
+		std::cout << '\n';
 	}
+	// Return the karnaugh map in the vector type
 
-	// Return the truth table in the vector type
 
-	std::cout << std::endl << "Simplify: " << Boolean::simplify(input) << std::endl;
-
+	std::cout << '\n' << "Simplify: " << Boolean::simplify(input) << '\n';
 	// Return simplified expression
 
-	std::cout << std::endl << "Zhegalkin polynomial: " << Boolean::polynom(input);
 
+	std::cout << '\n' << "Zhegalkin polynomial: " << Boolean::polynom(input) << '\n';
 	// Return Zhegalkin polynomial
 
 }
